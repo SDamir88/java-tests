ab384a474d170e2988732fe7b951eb7eab005b72
sitdi <sitdikov.88@inbox.ru>
2022-06-23 14:19:48 +0300

    Initial commit

---
+++ src/main/java/practice/OperatorsAndDataTypesTests.java
@@ -0,0 +1,36 @@
+package practice;
+
+public class OperatorsAndDataTypesTests {
+
+    public static void main(String [] args){
+
+        int a, b, c;
+        float d;
+        byte e;
+        boolean f = false, g = true;
+        boolean h = false, i = false;
+        a = 13;
+        b = 47;
+        c = 3;
+        d = 87.5F;
+        e = -128;
+        a += e;
+        b /= c %= e;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("c - d = " + (c - d));
+        System.out.println("a * b / e = " + (a * b / e));
+        System.out.println("b / a % c = " + (b / a % c));
+        System.out.println("e-- =" + (e--));
+        System.out.println("--e =" + (--e));
+        System.out.println("b == d =" + (b == d));
+        System.out.println("c != e =" + (c != e));
+
+        System.out.println("f && g && h = " + (f && g && h));
+        System.out.println("g || h && i = " + (g || h && i) );
+        System.out.println("!(h && i) || f = " + (!(h && i) || f));
+
+        System.out.println("a += e =" + a);
+        System.out.println("b /= c %= e =" + b);
+    }
+}
